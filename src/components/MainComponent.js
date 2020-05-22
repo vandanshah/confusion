cd074e3d92692a70b34c095a9eaae7b1f2eefee9
Vandan Shah <vandanshah235@gmail.com>
2020-05-21 20:16:06 -0700

    Added About Us Component.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import About from './AboutComponent';
 import Contact from './ContactComponent';
 import DishdetailedComponent from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
@@ -57,6 +58,7 @@ class Main extends Component {
           <Route exact path= "/menu" component = {() => <Menu dishes={this.state.dishes} />} />
           <Route path = "/menu/:dishId" component = {DishWithId} /> 
           <Route path = "/contactus" component={Contact} />
+          <Route path = "/aboutus" component = {() => <About leaders = {this.state.leaders} />} />
           <Redirect to= '/home'/>
         </Switch>
         <Footer />
